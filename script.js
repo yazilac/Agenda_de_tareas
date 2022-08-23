1f5e4bd4585f06b17269a6f70ed26ef9aac032c0
Yazmin <yazilac@gmail.com>
2022-08-23 12:45:23 -0400

    clase03 completada

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ const btn =  document.querySelector('[data-form-btn]');
 const createTask = (evento)=>{
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const value = input.value;
+    const task = document.createElement('li');
+    const list = document.querySelector("[data-list]");
+    task.classList.add('card');
+    //backtics
+    const content = ` <div>
+                    <i class="far fa-check-square icon"></i>
+                    <span class="task">${value}</span>
+                    </div>
+                    <i class="fas fa-trash-alt trashIcon icon"></i> `;
+    task.innerHTML = content;
+    list.appendChild(task);
+    input.value = "";
+
+    console.log(content);
 }
 
 console.log(btn);
